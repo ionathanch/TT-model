babbc10810f849be3363786757aadf5261c0a9a2
Jonathan Chan <jonathanhwchan@gmail.com>
2025-01-04 09:51:24 -0800

    lake build should now build the normalization files instead of the original consistency proofs

--- Main.lean
+++ Main.lean
@@ -1,10 +1,14 @@
 import «TT-model».level
 import «TT-model».syntactics
 import «TT-model».reduction
+import «TT-model».normal
 import «TT-model».typing
-import «TT-model».semantics
-import «TT-model».soundness
-import «TT-model».example
+import «TT-model».candidates
+import «TT-model».normalization
+import «TT-model».canonicity
+-- import «TT-model».semantics
+-- import «TT-model».soundness
+-- import «TT-model».example
 
 def main : IO Unit :=
   pure ()
